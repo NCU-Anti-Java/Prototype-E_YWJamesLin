7259f67a783c8e06174a7ea47d2a6fa1a350c169
YWJamesLin <JamesLin@ssinrc.org>
2015-10-30 21:55:44 +0800

    Prepare for Branching

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by YWJamesLin on 2015/10/30.
+ */
+import java.lang.String;
+import javax.swing.*;
+
+public class Main extends javax.swing.JFrame {
+    public static void main(String args[]) {
+        new Main();
+    }
+
+    public Main () {
+        this.setTitle("Fuck");
+        this.setSize(200, 200);
+        this.setVisible(true);
+    }
+}
